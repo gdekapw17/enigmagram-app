495c62d2dad05e64d92495480513f3657589df96
Eka Purwadi <gdeka1734@gmail.com>
2025-07-29 15:35:27 +0800

    feat: Add route structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
-
 import AuthLayout from './_auth/AuthLayout';
 import SigninForm from './_auth/forms/SigninForm';
 import SignupForm from './_auth/forms/SignupForm';
 import RootLayout from './_root/RootLayout';
-import { Home } from './_root/pages';
+import {
+  Home,
+  PostDetails,
+  AllUsers,
+  CreatePost,
+  EditPost,
+  Explore,
+  Profile,
+  Saved,
+  UpdateProfile,
+} from './_root/pages';
 import { Toaster } from './components/ui/toaster';
 
 const App = () => {
@@ -20,6 +29,14 @@ const App = () => {
         {/* Private Routes */}
         <Route element={<RootLayout />}>
           <Route index element={<Home />} />
+          <Route path="/explore" element={<Explore />}></Route>
+          <Route path="/saved" element={<Saved />}></Route>
+          <Route path="/all-users" element={<AllUsers />}></Route>
+          <Route path="/create-post" element={<CreatePost />}></Route>
+          <Route path="/update-pos:id" element={<EditPost />}></Route>
+          <Route path="/posts/:id" element={<PostDetails />}></Route>
+          <Route path="/profile/:id/*" element={<Profile />}></Route>
+          <Route path="/update-profile/:id" element={<UpdateProfile />}></Route>
         </Route>
       </Routes>
 
